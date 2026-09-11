a780d72331abed3127d1b43cba3dc7457881b9b5
dawinsight-dwiagus <>
2026-09-11 16:47:26 +0700

    feat: add open graph meta tags to homepage

--- assets/js/global/config.js
+++ assets/js/global/config.js
@@ -1,3 +1,3 @@
 // KONFIGURASI GLOBAL
 // Set link Google Apps Script (GAS) Web App di bawah ini untuk menghubungkan frontend ke backend spreadsheet.
-const API_URL = 'https://script.google.com/macros/s/AKfycbzBn0PtpPleLFXiQJimtVR17nFDAGY4dUNbZvxU4JQ2vAF6Qi_-W795NKZaeZONn5nJ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwshkughvRT_ag3-dL_W5VpfOiTMTfRknTNP-4xltGAfYCXXT4ZDG6hOyKh7hoBcDyl2w/exec';
