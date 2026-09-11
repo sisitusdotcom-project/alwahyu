a58f6202f428c658fa6da793b4efaf4e805b17f6
dawinsight-dwiagus <>
2026-09-11 09:42:19 +0700

    update

--- assets/js/global/config.js
+++ assets/js/global/config.js
@@ -1,3 +1,3 @@
 // KONFIGURASI GLOBAL
 // Set link Google Apps Script (GAS) Web App di bawah ini untuk menghubungkan frontend ke backend spreadsheet.
-const API_URL = 'https://script.google.com/macros/s/AKfycbxz0bRXDMXY8IAeVTckkhiM8o9n5jZEvgGIfW4E2bAERxhG9Nu8f740uAAudNup1pUELA/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzBn0PtpPleLFXiQJimtVR17nFDAGY4dUNbZvxU4JQ2vAF6Qi_-W795NKZaeZONn5nJ/exec';
