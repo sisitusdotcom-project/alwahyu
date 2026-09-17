4be0057c493a25f0a862408cee68d5fed870760a
dawinsight-dwiagus <>
2026-09-18 06:27:25 +0700

    update

--- assets/js/global/main.js
+++ assets/js/global/main.js
@@ -115,3 +115,15 @@
       }
     }
   });
+
+
+// Preloader logic
+window.addEventListener('load', function() {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.classList.add('hidden');
+    setTimeout(() => {
+      preloader.remove();
+    }, 500);
+  }
+});
